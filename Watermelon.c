8181146016a3d625be133af39a450e01555f1b8e
Anwar <anhossen97@gmail.com>
2024-11-06 21:51:14 +0600

    2nd Day

---
+++ Watermelon.c
@@ -0,0 +1,22 @@
+//https://codeforces.com/problemset/problem/4/A
+
+#include<stdio.h>
+ 
+int main() {
+ 
+    int w;
+ 
+    scanf("%d",&w);
+ 
+    if(w%2==0 && w>2){
+ 
+        printf("YES\n");
+    }
+ 
+    else{
+ 
+        printf("NO\n");
+    }
+ 
+    return 0;
+}

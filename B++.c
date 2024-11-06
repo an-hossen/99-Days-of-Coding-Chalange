e8fafbb5f30ebdec0ecf5d050b4d84cd60fd0815
Anwar <anhossen97@gmail.com>
2024-11-06 22:05:17 +0600

    5th Day

---
+++ B++.c
@@ -0,0 +1,32 @@
+//https://codeforces.com/problemset/problem/282/A
+
+
+#include <stdio.h>
+#include<string.h>
+
+int main() {
+   int n,x = 0,i;
+   
+   scanf("%d",&n);
+   
+   for(i=0;i<n;i++) {
+       
+      char s[100];
+      
+      scanf("%s",&s);
+      
+      if(strstr(s,"++")) {
+          
+          x++;
+      }
+      
+      else if(strstr(s,"--")) {
+          
+          x--;
+      }
+   }
+   
+   printf("%d\n",x);
+   
+    return 0;
+}

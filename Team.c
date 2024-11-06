b21029ba8a8f5db6364e17e5df23fa7ff7fd4e79
Anwar <anhossen97@gmail.com>
2024-11-06 22:05:47 +0600

    6th Day

---
+++ Team.c
@@ -0,0 +1,30 @@
+//https://codeforces.com/problemset/problem/231/A
+
+#include<stdio.h>
+
+int main() {
+    int n,i,count,solution=0;
+    
+    scanf("%d",&n);
+    
+    for( i = 0; i<n; i++) {
+        
+        int s[101],count = 0;
+        
+        for(int j=0;j<3;j++) {
+            
+            scanf("%d",&s[j]);
+            
+            if(s[j]==1) {
+                
+                count++;
+            }
+        }
+        if(count>1) {
+            solution++;
+        }
+    }
+    printf("%d\n",solution);
+    return 0;
+
+}
